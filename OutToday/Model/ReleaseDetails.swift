7b0d00b4c9df4f33dbaa019098dd8aaefaa9e463
Natalia Braz <naabraz@gmail.com>
2020-10-11 18:01:08 -0300

    feat(model): add release model

---
+++ OutToday/Model/ReleaseDetails.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public struct ReleaseDetails {
+  public let date: Date
+  public let title: String
+  public let producers: String
+  public let studio: String
+  public let recorded: String
+  public let url: URL?
+  
+  init(date: Date, title: String, producers: String, studio: String, recorded: String) {
+    self.date = date
+    self.title = title
+    self.producers = producers
+    self.studio = studio
+    self.recorded = recorded
+    self.url = URL(string: "emoji://\(title.filter { !$0.isWhitespace })")
+  }
+}
+
+extension ReleaseDetails: Identifiable {
+  public var id: String {
+    title
+  }
+}
