451fceb55d53d5866c775f5cf9c36b63fe5afb34
Natalia Braz <naabraz@gmail.com>
2020-10-11 17:34:37 -0300

    Initial Commit

---
+++ OutToday/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OutToday
+//
+//  Created by Natalia Braz on 11/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OutToday/OutTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutTodayApp.swift
+//  OutToday
+//
+//  Created by Natalia Braz on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct OutTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
