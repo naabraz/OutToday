941474cdc7b3480dd57ebe4f1d52c57f7f584c1e
Natalia Braz <naabraz@gmail.com>
2020-10-11 20:12:07 -0300

    feat(provider): create method to get all releases

--- OutToday/Data/ReleaseProvider.swift
+++ OutToday/Data/ReleaseProvider.swift
@@ -3,7 +3,21 @@ import Foundation
 public struct ReleaseProvider {
   static func getRelease() -> ReleaseDetails {
     let date = DateHelper.formatDate()
-    
+
     return Releases.allReleases[date] ?? Releases.emptyRelease
+  }
+  
+  static func getAllReleases() -> [ReleaseDetails] {
+    let releases = Releases.allReleases.map() {
+      ReleaseDetails(
+        date: "\($0.value.date)",
+        title: "\($0.value.title)",
+        producers: "\($0.value.producers)",
+        studio: "\($0.value.studio)",
+        recorded: "\($0.value.recorded)"
+      )
+    }
+    
+    return releases
   }
 }
