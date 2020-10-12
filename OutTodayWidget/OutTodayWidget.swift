e56cf5b4d3f48ec160156093ca2088c524e68677
Natalia Braz <naabraz@gmail.com>
2020-10-12 17:55:48 -0300

    chore(widget): refreshing every minute

--- OutTodayWidget/OutTodayWidget.swift
+++ OutTodayWidget/OutTodayWidget.swift
@@ -5,23 +5,23 @@ struct OutTodayWidgetProvider: TimelineProvider {
   func placeholder(in context: Context) -> OutTodayWidgetEntry {
     OutTodayWidgetEntry(date: Date(), release: ReleaseProvider.getRelease())
   }
-  
+
   func getSnapshot(in context: Context, completion: @escaping (OutTodayWidgetEntry) -> ()) {
     let entry = OutTodayWidgetEntry(date: Date(), release: ReleaseProvider.getRelease())
     completion(entry)
   }
-  
+
   func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
     var entries: [OutTodayWidgetEntry] = []
-    
-    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+
+    // Generate a timeline consisting of five entries an minute apart, starting from the current date.
     let currentDate = Date()
     for hourOffset in 0 ..< 5 {
-      let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+      let entryDate = Calendar.current.date(byAdding: .minute, value: hourOffset, to: currentDate)!
       let entry = OutTodayWidgetEntry(date: entryDate, release: ReleaseProvider.getRelease())
       entries.append(entry)
     }
-    
+
     let timeline = Timeline(entries: entries, policy: .atEnd)
     completion(timeline)
   }
@@ -34,7 +34,7 @@ struct OutTodayWidgetEntry: TimelineEntry {
 
 struct OutTodayWidgetEntryView : View {
   var entry: OutTodayWidgetProvider.Entry
-  
+
   var body: some View {
     OutTodaySmallWidget(release: entry.release)
   }
@@ -44,7 +44,7 @@ struct OutTodayWidgetEntryView : View {
 @main
 struct OutTodayWidget: Widget {
   let kind: String = "OutTodayWidget"
-  
+
   var body: some WidgetConfiguration {
     StaticConfiguration(kind: kind, provider: OutTodayWidgetProvider()) { entry in
       OutTodayWidgetEntryView(entry: entry)
