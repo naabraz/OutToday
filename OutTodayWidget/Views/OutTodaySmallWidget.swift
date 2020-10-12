59146ec457e05ece2df6787d8a27e896174a3361
Natalia Braz <naabraz@gmail.com>
2020-10-12 18:15:00 -0300

    feat(widget): add label to show number of releases

--- OutTodayWidget/Views/OutTodaySmallWidget.swift
+++ OutTodayWidget/Views/OutTodaySmallWidget.swift
@@ -1,12 +1,28 @@
 import SwiftUI
 
+struct NumberOfReleases: View {
+  let numberOfReleases: Int
+  
+  var body: some View {
+    ZStack {
+      Text(String(numberOfReleases))
+        .bold()
+        .padding(6)
+        .foregroundColor(.black)
+    }.background(Color.white)
+    .cornerRadius(8)
+    .padding(8)
+  }
+}
+
 struct OutTodaySmallWidget: View {
   let release: ReleaseDetails
-
+  
   var body: some View {    
     HStack {
       Image(release.image)
         .resizable()
+        .overlay(NumberOfReleases(numberOfReleases: 2), alignment: .bottomTrailing)
     }
   }
 }
