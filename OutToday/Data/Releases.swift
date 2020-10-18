eed6b86a8ff2a2c7f81c087c1f4b3d4108a70af4
Natalia Braz <naabraz@gmail.com>
2020-10-18 15:07:22 -0300

    feat(releases): add studio albums

--- OutToday/Data/Releases.swift
+++ OutToday/Data/Releases.swift
@@ -17,6 +17,132 @@ public struct Releases {
       recorded: "Compass Point Studios, The Cave Studios",
       image: "album-final-frontier",
       key: "1608"
+    ),
+    ReleaseDetails(
+      date: "28 August 2006",
+      title: "A Matter of Life and Death",
+      producers: "Kevin Shirley, Steve Harris",
+      studio: "EMI, Sanctuary (US)",
+      recorded: "Sarm West Studios",
+      image: "album-a-matter-of-life-and-death",
+      key: "2808"
+    ),
+    ReleaseDetails(
+      date: "08 September 2003",
+      title: "Dance of Death",
+      producers: "Kevin Shirley, Steve Harris",
+      studio: "EMI, Columbia Records (US)",
+      recorded: "Sarm West Studios",
+      image: "album-dance-of-death",
+      key: "0809"
+    ),
+    ReleaseDetails(
+      date: "29 May 2000",
+      title: "Brave New World",
+      producers: "Kevin Shirley, Steve Harris",
+      studio: "EMI, Portrait / Columbia (United States)",
+      recorded: "Guillaume Tell Studios",
+      image: "album-brave-new-world",
+      key: "2905"
+    ),
+    ReleaseDetails(
+      date: "23 March 1998",
+      title: "Virtual XI",
+      producers: "Steve Harris, Nigel Green",
+      studio: "EMI, CMC / BMG (US)",
+      recorded: "Barnyard Studios",
+      image: "album-virtual-xi",
+      key: "2303"
+    ),
+    ReleaseDetails(
+      date: "02 October 1995",
+      title: "The X Factor",
+      producers: "Steve Harris, Nigel Green",
+      studio: "EMI, CMC (North America)",
+      recorded: "Barnyard Studios",
+      image: "album-the-x-factor",
+      key: "0210"
+    ),
+    ReleaseDetails(
+      date: "11 May 1992",
+      title: "Fear of the Dark",
+      producers: "Martin Birch, Steve Harris",
+      studio: "EMI, Epic (US)",
+      recorded: "Barnyard Studios",
+      image: "album-fear-of-the-dark",
+      key: "1105"
+    ),
+    ReleaseDetails(
+      date: "01 October 1990",
+      title: "No Prayer for the Dying",
+      producers: "Martin Birch",
+      studio: "EMI, Epic (US)",
+      recorded: "Barnyard Studios",
+      image: "album-no-prayer-for-the-dying",
+      key: "0110"
+    ),
+    ReleaseDetails(
+      date: "11 April 1988",
+      title: "Seventh Son of a Seventh Son",
+      producers: "Martin Birch",
+      studio: "EMI, Capitol (US)",
+      recorded: "Musicland Studios",
+      image: "album-seventh-son",
+      key: "1104"
+    ),
+    ReleaseDetails(
+      date: "29 September 1986",
+      title: "Somewhere in Time",
+      producers: "Martin Birch",
+      studio: "EMI, Capitol (North America)",
+      recorded: "Compass Point Studios, Wisseloord Studios",
+      image: "album-somewhere-in-time",
+      key: "2909"
+    ),
+    ReleaseDetails(
+      date: "03 September 1984",
+      title: "Powerslave",
+      producers: "Martin Birch",
+      studio: "EMI, Capitol (North America)",
+      recorded: "Compass Point Studios",
+      image: "album-power-slave",
+      key: "0309"
+    ),
+    ReleaseDetails(
+      date: "16 May 1983",
+      title: "Piece of Mind",
+      producers: "Martin Birch",
+      studio: "EMI, Capitol (North America)",
+      recorded: "Compass Point Studios",
+      image: "album-piece-of-mind",
+      key: "1605"
+    ),
+    ReleaseDetails(
+      date: "22 March 1982",
+      title: "The Number of the Beast",
+      producers: "Martin Birch",
+      studio: "EMI (EMC 3400), Harvest (North America)",
+      recorded: "Battery Studios",
+      image: "album-number-of-the-beast",
+      key: "2203"
+    ),
+    ReleaseDetails(
+      date: "02 February 1981",
+      title: "Killers",
+      producers: "Martin Birch",
+      studio: "EMI, Harvest / Capitol (North America)",
+      recorded: "Battery Studios",
+      image: "album-killers",
+      key: "0202"
+    ),
+    ReleaseDetails(
+      date: "14 April 1980",
+      title: "Iron Maiden",
+      producers: "Will Malone",
+      studio: "EMI (Europe), Harvest Capitol (North America)",
+      recorded: "Kingsway Studios",
+      image: "album-iron-maiden",
+      key: "1404"
     )
   ]
 }
