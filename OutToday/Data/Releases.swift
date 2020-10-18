e6712cdc070497c06bed213562457c80c274b963
Natalia Braz <naabraz@gmail.com>
2020-10-18 16:36:04 -0300

    feat(releases): add singles

--- OutToday/Data/Releases.swift
+++ OutToday/Data/Releases.swift
@@ -197,6 +197,222 @@ public struct Releases {
       title: "The Trooper",
       image: "single-1983-the-trooper",
       key: "2006"
+    ),
+    ReleaseDetails(
+      date: "06 August 1984",
+      title: "2 Minutes To Midnight",
+      image: "single-1984-2-minutes-to-midnight",
+      key: "0608"
+    ),
+    ReleaseDetails(
+      date: "22 October 1984",
+      title: "Aces High",
+      image: "single-1984-aces-high",
+      key: "2210"
+    ),
+    ReleaseDetails(
+      date: "23 September 1985",
+      title: "Running Free (Live)",
+      image: "single-1985-running-free-live",
+      key: "2309"
+    ),
+    ReleaseDetails(
+      date: "02 December 1985",
+      title: "Run To The Hills (Live)",
+      image: "single-1985-run-to-the-hills-live",
+      key: "0212"
+    ),
+    ReleaseDetails(
+      date: "06 September 1986",
+      title: "Wasted Years",
+      image: "single-1986-wasted-years",
+      key: "0609"
+    ),
+    ReleaseDetails(
+      date: "22 November 1986",
+      title: "Stranger In A Strange Land",
+      image: "single-1986-stranger-in-a-strange-land",
+      key: "2211"
+    ),
+    ReleaseDetails(
+      date: "20 March 1988",
+      title: "Can I Play With Madness",
+      image: "single-1988-can-i-play-with-madness",
+      key: "2003"
+    ),
+    ReleaseDetails(
+      date: "01 August 1988",
+      title: "The Evil That Men Do",
+      image: "single-1988-evil-that-men-do",
+      key: "0108"
+    ),
+    ReleaseDetails(
+      date: "07 November 1988",
+      title: "The Clairvoyant",
+      image: "single-1988-clairvoyant",
+      key: "0711"
+    ),
+    ReleaseDetails(
+      date: "06 November 1989",
+      title: "Infinite Dreams (Live)",
+      image: "single-1989-infinite-dreams-7",
+      key: "0611"
+    ),
+    ReleaseDetails(
+      date: "10 September 1990",
+      title: "Holy Smoke",
+      image: "single-1990-holy-smoke",
+      key: "1009"
+    ),
+    ReleaseDetails(
+      date: "24 December 1990",
+      title: "Bring Your Daughter...To The Slaughter",
+      image: "single-1990-bring-your-daughter",
+      key: "2412"
+    ),
+    ReleaseDetails(
+      date: "13 April 1992",
+      title: "Be Quick Or Be Dead",
+      image: "single-1992-be-quick-or-dead",
+      key: "1304"
+    ),
+    ReleaseDetails(
+      date: "29 June 1992",
+      title: "From Here To Eternity",
+      image: "single-1992-here-to-eternity",
+      key: "2906"
+    ),
+    ReleaseDetails(
+      date: "01 September 1992",
+      title: "Wasting Love",
+      image: "single-1992-wasting-love",
+      key: "0109"
+    ),
+    ReleaseDetails(
+      date: "01 March 1993",
+      title: "Fear Of The Dark (Live)",
+      image: "single-1993-fear-of-the-dark-live",
+      key: "0103"
+    ),
+    ReleaseDetails(
+      date: "04 October 1993",
+      title: "Hallowed Be Thy Name (Live)",
+      image: "single-1993-hallowed-be-thy-name",
+      key: "0410"
+    ),
+    ReleaseDetails(
+      date: "25 September 1995",
+      title: "Man On The Edge",
+      image: "single-1995-man-on-the-edge",
+      key: "2509"
+    ),
+    ReleaseDetails(
+      date: "02 February 1996",
+      title: "Lord Of The Flies",
+      image: "single-1996-lord-of-the-flies",
+      key: "0202"
+    ),
+    ReleaseDetails(
+      date: "02 September 1996",
+      title: "Virus",
+      image: "single-1996-virus",
+      key: "0209"
+    ),
+    ReleaseDetails(
+      date: "28 July 1998",
+      title: "Futureal",
+      image: "single-1998-futureal",
+      key: "2807"
+    ),
+    ReleaseDetails(
+      date: "09 March 1998",
+      title: "The Angel And The Gambler",
+      image: "single-1998-gambler",
+      key: "0903"
+    ),
+    ReleaseDetails(
+      date: "08 May 2000",
+      title: "The Wicker Man",
+      image: "single-2000-wickerman",
+      key: "0805"
+    ),
+    ReleaseDetails(
+      date: "23 October 2000",
+      title: "Out Of The Silent Planet",
+      image: "single-2000-silent-planet",
+      key: "2310"
+    ),
+    ReleaseDetails(
+      date: "25 March 2002",
+      title: "Run To The Hills",
+      image: "single-2002-run-to-the-hills",
+      key: "2503"
+    ),
+    ReleaseDetails(
+      date: "01 September 2003",
+      title: "Wildest Dreams",
+      image: "single-2003-wildest-dreams",
+      key: "0109"
+    ),
+    ReleaseDetails(
+      date: "24 November 2003",
+      title: "Rainmaker",
+      image: "single-2003-rainmaker",
+      key: "2411"
+    ),
+    ReleaseDetails(
+      date: "29 March 2004",
+      title: "No More Lies",
+      image: "single-2004-no-more-lies",
+      key: "2903"
+    ),
+    ReleaseDetails(
+      date: "03 January 2005",
+      title: "The Number Of The Beast",
+      image: "single-2005-number-of-the-beast",
+      key: "0301"
+    ),
+    ReleaseDetails(
+      date: "15 August 2005",
+      title: "The Trooper",
+      image: "single-2005-the-trooper",
+      key: "1508"
+    ),
+    ReleaseDetails(
+      date: "14 August 2006",
+      title: "The Reincarnation Of Benjamin Breeg",
+      image: "single-2006-breeg",
+      key: "1408"
+    ),
+    ReleaseDetails(
+      date: "25 December 2006",
+      title: "Different World",
+      image: "single-2006-different-world",
+      key: "2512"
+    ),
+    ReleaseDetails(
+      date: "08 June 2010",
+      title: "El Dorado",
+      image: "single-2010-eldorado",
+      key: "0806"
+    ),
+    ReleaseDetails(
+      date: "13 July 2010",
+      title: "Satellite 15... The Final Frontier",
+      image: "single-2010-satellite",
+      key: "1307"
+    ),
+    ReleaseDetails(
+      date: "14 August 2015",
+      title: "Speed of Light",
+      image: "single-2015-speed-of-light",
+      key: "1408"
+    ),
+    ReleaseDetails(
+      date: "16 April 2016",
+      title: "Empire of the Clouds",
+      image: "single-2016-empire-of-the-clouds",
+      key: "1604"
     )
   ]
 }
