626cc542ec1d081a74953872f6621a59776e5d92
Natalia Braz <naabraz@gmail.com>
2020-10-24 15:49:42 -0300

    feat(next release): create function to get nearest day

--- OutToday/Helpers/Date.swift
+++ OutToday/Helpers/Date.swift
@@ -1,6 +1,27 @@
 import Foundation
 
 public struct DateHelper {
+  static func getClosestDate() -> String {
+    let date = Date()
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy/MM/dd"
+    
+    let calendar = Calendar.current
+    let year = String(calendar.component(.year, from: date))
+    
+    let date1 = formatter.date(from: year+"/12/13")
+    let date2 = formatter.date(from: year+"/01/08")
+    let date3 = formatter.date(from: year+"/03/15")
+    
+    let dateList = [date1!, date2!, date3!]
+    
+    if let closestDate = dateList.sorted().first(where: {$0.timeIntervalSinceNow > 0}) {
+      return String(closestDate.description(with: .current))
+    }
+    
+    return "No closest date"
+  }
+    
   static func formatDate() -> String {
     let date = Date()
     let dateFormatter = DateFormatter()
@@ -9,6 +30,8 @@ public struct DateHelper {
     dateFormatter.setLocalizedDateFormatFromTemplate("dd-MM")
     
     let formattedDate = dateFormatter.string(from: date).replacingOccurrences(of: "/", with: "")
+    
+    print("closestDate", getClosestDate())
     
     return formattedDate
   }
