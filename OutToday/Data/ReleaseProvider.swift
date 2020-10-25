901b6dd80e99c2c858b3423d892e868abe083e0f
Natalia Braz <naabraz@gmail.com>
2020-10-25 16:24:51 -0300

    feat(next release): format next date

--- OutToday/Data/ReleaseProvider.swift
+++ OutToday/Data/ReleaseProvider.swift
@@ -1,10 +1,38 @@
 import Foundation
 
 public struct ReleaseProvider {
+  static func getClosestDate() -> Void {
+    let releases = Releases.allReleases
+    var releaseDates = [] as [Date]
+    
+    let formatter = DateFormatter()
+    formatter.dateFormat = "dd/MM/yyyy"
+
+    let date = Date()
+    let calendar = Calendar.current
+    let year = String(calendar.component(.year, from: date))
+    
+    releases.forEach { release in
+      let releaseDateKey = String(release.key.enumerated().map { $0 > 0 && $0 % 2 == 0 ? ["/", $1] : [$1]}.joined())
+      
+      releaseDates.append(formatter.date(from: releaseDateKey+"/"+year)!)
+    }
+
+    if let closestDate = releaseDates.sorted().first(where: {$0.timeIntervalSinceNow > 0}) {
+      let closest = String(closestDate.description).prefix(10)
+      let dateArray = closest.split{$0 == "-"}.map(String.init)
+      let key = dateArray[2]+dateArray[1]
+
+      print("closestDate ", key)
+    }
+  }
+
   static func getRandomRelease() -> RandomReleaseofTheDay {
     let date = DateHelper.formatDate()
     let releases = Releases.allReleases
     var releasesOfTheDay = [ReleaseDetails]()
+    
+    getClosestDate()
     
     releases.forEach { release in
       if(release.key == date) {
@@ -27,7 +55,7 @@ public struct ReleaseProvider {
       release: releasesOfTheDay[randomIndex],
       numberOfReleases: numberOfReleases
     )
-    
+
     return randomRelease
   }
 }
--- OutToday/Helpers/Date.swift
+++ OutToday/Helpers/Date.swift
@@ -1,33 +1,8 @@
 import Foundation
 
 public struct DateHelper {
-  static func getClosestDate() -> Void {
-    let releases = Releases.allReleases
-    var releaseDates:[Date] = []
-    
-    let date = Date()
-    let calendar = Calendar.current
-    let year = String(calendar.component(.year, from: date))
-    
-    releases.forEach { release in
-      let formatter = DateFormatter()
-      formatter.dateFormat = "dd/MM/yyyy"
-      
-      let releaseDateKey = String(release.key.enumerated().map { $0 > 0 && $0 % 2 == 0 ? ["/", $1] : [$1]}.joined())
-      
-      releaseDates.append(formatter.date(from: releaseDateKey+"/"+year)!)
-    }
-        
-    if let closestDate = releaseDates.sorted().first(where: {$0.timeIntervalSinceNow > 0}) {
-      let closest = String(closestDate.description(with: .current))
 
-      print("closestDate ", closest)
-    }
-  }
-    
   static func formatDate() -> String {
-    getClosestDate()
-
     let date = Date()
     let dateFormatter = DateFormatter()
     
